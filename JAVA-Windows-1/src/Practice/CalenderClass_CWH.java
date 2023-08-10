f593e4c62ec13e31182099854e6aa4e92c825842
Saptarshi Sarkar <saptarshi2017cob@gmail.com>
2023-08-10 22:47:29 +0530

    10th Aug 2023, made a new repo for java practice as getting error in the previous setup. Now will practice Java and Dsa in this repo.

--- JAVA-Windows-1/src/Practice/CalenderClass_CWH.java
+++ JAVA-Windows-1/src/Practice/CalenderClass_CWH.java
@@ -12,6 +12,15 @@ public class CalenderClass_CWH {
         System.out.println(c.getTime());
         System.out.println(c.getTimeZone().getID());
 
+        //methods
+        Calendar cal = Calendar.getInstance();
+        System.out.println(cal.getTime());
+        System.out.println(cal.get(Calendar.DATE));
+        System.out.println(cal.get(Calendar.SECOND));
+        System.out.println(cal.get(Calendar.MINUTE));
+        System.out.println(cal.get(Calendar.HOUR));  // 12-hour format
+        System.out.println(cal.get(Calendar.HOUR_OF_DAY)); // 24-hour format
+
         // For Diff Time Zone
         Calendar cc = Calendar.getInstance(TimeZone.getTimeZone("Asia/Singapore"));
         System.out.println(cc.getCalendarType());
---
+++ JAVA-Windows-1/src/Practice/GregorianCalender_CWH.java
@@ -0,0 +1,11 @@
+package Practice;
+
+
+import java.util.GregorianCalendar;
+
+public class GregorianCalender_CWH {
+    public static void main(String[] args) {
+        GregorianCalendar gc = new GregorianCalendar();
+        System.out.println(gc.getTime());
+    }
+}
