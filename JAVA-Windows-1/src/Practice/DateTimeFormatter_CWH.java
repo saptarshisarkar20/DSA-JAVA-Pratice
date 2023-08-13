ee07e55e6083116582986c8dbf1d8b4734f3d059
Saptarshi Sarkar <saptarshi2017cob@gmail.com>
2023-08-13 22:12:16 +0530

    13th Aug 2023 -> After coming back to house a small tour #Pashakha #TorsaRiverSideCafe

---
+++ JAVA-Windows-1/src/Practice/DateTimeFormatter_CWH.java
@@ -0,0 +1,19 @@
+package Practice;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateTimeFormatter_CWH {
+    public static void main(String[] args) {
+        LocalDateTime dt = LocalDateTime.now(); //This is the date
+        System.out.println(dt);
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("DD | MM | YYYY --> E | H:m a ");  // This is the format
+        DateTimeFormatter dtf2 = DateTimeFormatter.ISO_ORDINAL_DATE;
+
+        String myDate2 = dt.format(dtf2);
+        String myDate = dt.format(dtf); // Creating the date string
+        System.out.println(myDate);
+        System.out.println(myDate2);
+    }
+}
