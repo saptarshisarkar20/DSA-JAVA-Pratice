b1e873e6e9162c137797aa0a4e4d88cb7089f961
Saptarshi Sarkar <saptarshi2017cob@gmail.com>
2024-03-12 01:20:13 +0530

    11th Mar 2024, today went to gym and started practicing, While enjoying energetic music used to listen in childhood. In office solved one bug and written some QA automation.

---
+++ CPP-Windows-1/src/LongestCommonPrefix.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <string>
+#include <cmath>
+using namespace std;
+
+// ############################
+
+string longestCommonPrefix(vector<string> &strs)
+{
+    int smSize = INT_MAX;
+
+    for (int i = 0; i < strs.size(); i++)
+    {
+        if (smSize > strs[i].length())
+        {
+            smSize = strs[i].length();
+        }
+    }
+    string ans = "";
+
+    for (int i = 0; i < smSize; i++)
+    {
+        string temp = strs[0][i];
+        for (int j = 1; j < strs.size(); j++)
+        {
+            if (temp != strs[j][i])
+                return ans;
+        }
+        ans += temp;
+    }
+
+    return ans;
+}
+
+// ############################
+
+int main()
+{
+
+    return 0;
+}
---
+++ JAVA-Windows-1/src/GFG/GeneralisedFibonacciNumbers.java
@@ -0,0 +1,2 @@
+package GFG;public class GeneralisedFibonacciNumbers {
+}
