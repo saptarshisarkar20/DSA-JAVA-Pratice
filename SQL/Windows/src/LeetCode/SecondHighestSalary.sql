71ba9818a6d73da0bf7b82c54bca9be949b43430
Saptarshi Sarkar <saptarshi2017cob@gmail.com>
2024-04-04 01:56:10 +0530

    4th April 2024, started practicing SQL

---
+++ SQL/Windows/src/LeetCode/SecondHighestSalary.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+SELECT
+    IFNULL(
+        (
+            SELECT
+                DISTINCT(salary)
+            FROM
+                Employee
+            ORDER BY
+                salary DESC
+            LIMIT
+                1 OFFSET 1
+        ),
+        NULL
+    ) AS SecondHighestSalary;
